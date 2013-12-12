dbbbe75eb06bc820f34439372715ad1dcb2a685c
tomaszb <tbacewicz@gmail.com>
2013-12-11 20:18:58 -0800

    added prototype pictures functionality to dashboard along with adding new pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,11 +2,21 @@ class PicturesController < ApplicationController
 
   def upload_picture
     picture = Picture.new
+    startup = Startup.find_by_id(params[:startup_id])
     picture.startup_id = params[:startup_id]
+
     picture.avatar = params[:avatar]
-    picture.status = 1
-    picture.title = 'new'
-    picture.description = "no description"
+    picture.status = startup.pictures.length + 1
+    if params[:pic_title].empty?
+      picture.title = "Picture #{picture.status}"
+    else
+      picture.title = params[:pic_title]
+    end
+    if params[:pic_description].empty?
+      picture.description = " "
+    else
+      picture.description = params[:pic_description]
+    end
 
     if picture.save!
       redirect_to :controller => "startups", :action => "dashboard", :id => picture.startup_id
