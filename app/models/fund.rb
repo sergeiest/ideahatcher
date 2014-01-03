f46aa4de03ce2c65ff5344064d74ad635e9fabbd
tomaszb <tbacewicz@gmail.com>
2014-01-02 16:00:13 -0800

    Fund has_many users through 'colleagues', not 'classoleagues'

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -3,6 +3,6 @@ class Fund < ActiveRecord::Base
   attr_accessible :name, :hashtag, :status
 
   has_many :colleagues, :dependent => :destroy
-  has_many :users, :through => :classolleagues
+  has_many :users, :through => :colleagues
 
 end
